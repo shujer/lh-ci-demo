767967340448cc1ec3c9f0967d08462bf8779f21
shujer <dwellerofuniverse@gmail.com>
2021-06-01 23:14:39 +0800

    feat: update ci

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,10 +1,16 @@
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: "./dist"
+      staticDistDir: "./build",
     },
     upload: {
-      target: "temporary-public-storage"
-    }
-  }
+      target: "temporary-public-storage",
+    },
+    assertions: {
+      "categories:performance": ["error", { minScore: 0.1 }],
+      "categories:accessibility": ["error", { minScore: 0.6 }],
+      "categories:best-practices": ["error", { minScore: 0.6 }],
+      "categories:seo": ["error", { minScore: 0.6 }],
+    },
+  },
 };
